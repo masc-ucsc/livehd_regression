618750e61714ce95437e8a7e95617e7f47d72503
Jose Renau <renau@ucsc.edu>
2021-01-14 15:14:42 -0800

    first release

---
+++ tools/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -f "firrtl/tools/bin/firrtl" ]; then
+  echo "FIRRTL seems setup"
+else
+  rm -rf firrtl*
+	curl -L -o firrtl.tar.gz https://github.com/chipsalliance/firrtl/archive/v1.4.1.tar.gz
+  tar xzvf firrtl.tar.gz 
+  mv firrtl-* firrtl
+  cp WritePB.scala firrtl/src/main/scala/firrtl/transforms/
+  cd firrtl
+  sbt compile
+  sbt assembly
+fi
