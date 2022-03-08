40ebca6557eb280009b8eee6f052850b56008d43
Mark Zakharov <markzakharov1102@gmail.com>
2022-03-07 22:36:02 -0800

    added simple examples

---
+++ HDLgen/comb_loop_examples/randomverilog3.v
@@ -0,0 +1,27 @@
+module randomverilog(
+
+	input clock,
+	input reset,
+	input signed [0:0] io_a0,
+	input signed [0:0] io_a1,
+	input signed [0:0] io_a2,
+	output [2:0] io_y);
+
+
+
+
+	wire [0:0] y0;
+	wire [0:0] y1;
+	wire [0:0] y2;
+
+
+
+	assign y0 = ($signed(~(io_a1[0]>io_a0[0])) + $signed((io_a1[0]>io_a0[0])));
+	assign y1 = (~($signed((y2[0]<io_a1[0])) + $signed(y0[0])));
+	assign y2 = (~(io_a1[0]>io_a0[0]))?$signed((!y1[0:0])):$signed(~(!io_a0[0:0]));
+
+	assign io_y = {y2,y1,y0};
+endmodule
+
+randomverilog.v:comb_loop:281:322
+randomverilog.v:comb_loop:342:397
