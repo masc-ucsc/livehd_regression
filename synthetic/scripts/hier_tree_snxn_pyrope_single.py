2110d1951630736a3d72c0e62a258f0922718d33
sheng hong wang <swang203@ucsc.edu>
2022-08-03 16:16:57 -0700

    new pyrope synthetic pattern with separated files

--- synthetic/scripts/hier_tree_snxn_pyrope_single.py
+++ synthetic/scripts/hier_tree_snxn_pyrope_single.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 from treelib import Node, Tree
-import os
 import sys
 import re
 import math
@@ -10,11 +9,11 @@ module2last_inv = {}
 def main():
     # total_cells_list = [100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000, 1000000]
     # total_cells_list = [1000000]
-    total_cells_list = [100]
+    total_cells_list = [1000000]
     # total_cells_list = [40]
     avg_children = 4
     # avg_module_size = 300  # small BOOM avg
-    avg_module_size = 4  # small BOOM avg
+    avg_module_size = 300  # small BOOM avg
     sd = 1658              # small BOOM SD
 
     # avg_children = 4
@@ -120,37 +119,51 @@ def main():
         #step-4: check the chidren module name, create children instantiations
         #step-5: create some interaction for the children output and the parent output
 
-        print("// total modules: %s\n" %(total_created_modules))
-        print("// design tree depth: %s\n" %(tree.depth()+1))
-        print("// design size: %s\n" %(total_assigned_cells))
-        print("// avg module size: %s\n" %(avg_module_size))
+        f = open("%s.prp" %(tree[tree.root].tag.split()[1]), "w+")
+        f.write("// total modules: %s\n" %(total_created_modules))
+        f.write("// design tree depth: %s\n" %(tree.depth()+1))
+        f.write("// design size: %s\n" %(total_assigned_cells))
+        f.write("// avg module size: %s\n" %(avg_module_size))
            
 
-        def gen_logic_chain(module_name, assigned_cells, f):
+        def gen_logic_chain(module_name, f, assigned_cells, d, is_top):
             global module2last_inv
+            # indent = ""
+            # if (depth != 0):
+            #     indent = "  " * depth * 2 
+
+
+            f.write("\n")
+            if (is_top == False):
+                f.write("    " * (d - 1) + "%s = || {\n" %(module_name))
+            # f.write("    " * d + "$a.__ubits = 2\n")
+            # f.write("    " * d + "$b.__ubits = 2\n")
+
+
             for i in range(assigned_cells):
                 if i == 0:
-                    f.write("t0    = $a + $b\n")
-                    f.write("inv0  = ~t0\n")
-                    f.write("x0    = t0 ^ inv0\n")
-                    f.write("invx0 = ~x0\n")
+                    f.write("    " * d + "t0    = $a + $b\n")
+                    f.write("    " * d + "inv0  = ~t0\n")
+                    f.write("    " * d + "x0    = t0 ^ inv0\n")
+                    f.write("    " * d + "invx0 = ~x0\n")
                     if (i == assigned_cells - 1):
                         module2last_inv[module_name] = "invx%d" %(i)
                 else:
-                    f.write("t%d    = x%d + invx%d\n"   %(i, i-1, i-1))
-                    f.write("inv%d  = ~t%d\n"           %(i, i))
-                    f.write("x%d    = t%d ^ inv%d\n"    %(i, i, i))
-                    f.write("invx%d = ~x%d\n"           %(i, i))
+                    f.write("    " * d + "t%d    = x%d + invx%d\n"   %(i, i-1, i-1))
+                    f.write("    " * d + "inv%d  = ~t%d\n"           %(i, i))
+                    f.write("    " * d + "x%d    = t%d ^ inv%d\n"    %(i, i, i))
+                    f.write("    " * d + "invx%d = ~x%d\n"           %(i, i))
                     # f.write(indent + "%%z = invx%d;\n"             %(i))
                     if (i == assigned_cells - 1):
                         module2last_inv[module_name] = "invx%d" %(i)
                     
         
         # create children instances
-        def handle_submodule(node, f):
+        def handle_submodule(node, f, d, is_top):
             global module2last_inv
             submodule_names = []
             for child in tree.children(node):
+                # print(child.tag)
                 submodule_name = ""
                 for word in child.tag.split():
                     if word.isdigit():
@@ -160,20 +173,21 @@ def main():
 
 
 
-                f_sub = open("%s.prp" %(submodule_name), "w+")
-                gen_logic_chain(submodule_name, assigned_cells, f_sub)
-                handle_submodule(submodule_name, f_sub)
+                print(submodule_name, assigned_cells) 
+                gen_logic_chain(submodule_name, f, assigned_cells, d, False)
+                handle_submodule(submodule_name, f, d + 1, False)
                 submodule_names.append(submodule_name)
 
 
             for submodule_name in submodule_names:
-                f.write("res_%s = %s(a = $a, b = $b)\n" %(submodule_name, submodule_name))
+                print(submodule_name)
+                f.write("    " * (d - 1) + "res_%s = %s(a = $a, b = $b)\n" %(submodule_name, submodule_name))
 
             if not submodule_names:
-                f.write("%%z = %s\n" %(module2last_inv[node]))
+                f.write("    " * (d - 1) + "%%z = %s\n" %(module2last_inv[node]))
             else:
                 f.write("\n")
-                f.write("sum = ")
+                f.write("    " * (d - 1) + "sum = ")
                 i = 0
                 for submodule_name in submodule_names:
                     if i == 0:
@@ -182,12 +196,14 @@ def main():
                         f.write(" + res_%s.z" %(submodule_name))
                     i = i + 1
                 f.write("\n")
-                f.write("%z = sum ^ $a")
+                f.write("    " * (d - 1) + "%z = sum ^ $a")
                 f.write("\n")
 
+            if (is_top == False):
+                f.write("    " * (d - 2) + "}\n\n")
 
 
-        f = open("%s.prp" %(tree[tree.root].tag.split()[1]), "w+")
+
         node = tree.root
         assigned_cells = 0
         module_name = ""
@@ -203,8 +219,9 @@ def main():
         global module2last_inv 
         depth = 0
 
-        gen_logic_chain(module_name, assigned_cells, f)
-        handle_submodule(node, f)
+        print(module_name, assigned_cells) 
+        gen_logic_chain(module_name, f, assigned_cells, depth, True)
+        handle_submodule(node, f, depth + 1, True)
 
 if __name__ == "__main__":
     main()
